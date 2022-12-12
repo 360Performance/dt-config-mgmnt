0c95cdf27c448d306b52e9fea76f089212de1202
Reinhard Weber <r.brandstaedter@gmail.com>
2022-12-12 15:08:17 +0100

    bug: more anomalyDetection entities with IDs

--- docker/configmanager/configtypes/config_v1/setting/anomalyDetectiondiskEvents.py
+++ docker/configmanager/configtypes/config_v1/setting/anomalyDetectiondiskEvents.py
@@ -4,6 +4,7 @@ from ...ConfigTypes import TenantConfigV1Entity, TenantConfigV1Setting
 class anomalyDetectiondiskEvents(TenantConfigV1Setting):
     entityuri = "/anomalyDetection/diskEvents"
     uri = TenantConfigV1Entity.uri + entityuri
+    has_id = True
 
     def __init__(self, **kwargs):
         TenantConfigV1Setting.__init__(self, **kwargs)
--- docker/configmanager/configtypes/config_v1/setting/anomalyDetectionprocessGroups.py
+++ docker/configmanager/configtypes/config_v1/setting/anomalyDetectionprocessGroups.py
@@ -4,6 +4,7 @@ from ...ConfigTypes import TenantConfigV1Entity, TenantConfigV1Setting
 class anomalyDetectionprocessGroups(TenantConfigV1Setting):
     entityuri = "/anomalyDetection/processGroups"
     uri = TenantConfigV1Entity.uri + entityuri
+    has_id = True
 
     def __init__(self, **kwargs):
         TenantConfigV1Setting.__init__(self, **kwargs)
