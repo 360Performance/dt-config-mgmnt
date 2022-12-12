c4080c16439420f4a1a1b5cac24926fe828a2a79
Reinhard Weber <r.brandstaedter@gmail.com>
2022-12-12 15:30:08 +0100

    bug: name attribute for conditionalNaming entities

--- docker/configmanager/configtypes/config_v1/conditionalNamingprocessGroup.py
+++ docker/configmanager/configtypes/config_v1/conditionalNamingprocessGroup.py
@@ -4,6 +4,7 @@ from ..ConfigTypes import TenantConfigV1Entity
 class conditionalNamingprocessGroup(TenantConfigV1Entity):
     entityuri = "/conditionalNaming/processGroup"
     uri = TenantConfigV1Entity.uri + entityuri
+    name_attr = "displayName"
 
     def setName(self, name):
         self.dto["displayName"] = self.name
