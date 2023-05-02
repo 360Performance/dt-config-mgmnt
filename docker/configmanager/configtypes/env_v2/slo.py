4a0cd7f4357daaf8a11f0062054e1d94b744033e
Reinhard Weber (Brandstaedter) <r.brandstaedter@gmail.com>
2023-05-02 16:04:30 +0200

    smart handling of SLO configs, using DT generated IDs (#47)

--- docker/configmanager/configtypes/env_v2/slo.py
+++ docker/configmanager/configtypes/env_v2/slo.py
@@ -1,6 +1,9 @@
 
 ''' Dynatrace Settings V2 API Support '''
 from ..ConfigTypes import TenantEnvironmentV2Entity
+import logging
+
+logger = logging.getLogger("slo")
 
 
 class slo(TenantEnvironmentV2Entity):
@@ -20,7 +23,19 @@ class slo(TenantEnvironmentV2Entity):
         self.apipath = self.uri+"/"+self.entityid
 
     def getHttpMethod(self):
-        return "POST" if self.entityid.startswith("0000") else "PUT"
+        return "POST"
 
     def getAll(self, session):
         pass
+
+    def post(self, dtapi, parameters={}):
+        # first get a potentially existing SLO with that exect Name (parameter sloSelector: name())
+        # then - if it exists - get it's ID and UPDATE the SLO with a PUT and the ID,
+        # else create a new one with a POST
+        slo = self.get(dtapi=dtapi, parameters=parameters | {"sloSelector": f'name({self.dto["name"]})'} )
+        if len(slo) > 0 and "slo" in slo[0] and len(slo[0]["slo"]) > 0:
+            slodef = slo[0]["slo"]
+            self.setID(slodef[0][self.id_attr])
+            super().put(dtapi,parameters)
+        else:
+            super().post(dtapi,parameters)
