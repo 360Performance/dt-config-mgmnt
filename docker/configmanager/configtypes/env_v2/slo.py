e62fccb41bd1559c2797331957639b9a08cb4f2b
Reinhard Weber <r.brandstaedter@gmail.com>
2023-05-02 14:26:52 +0200

    spceial case SLOs have an ID but can't be put with a custom ID

--- docker/configmanager/configtypes/env_v2/slo.py
+++ docker/configmanager/configtypes/env_v2/slo.py
@@ -8,11 +8,15 @@ class slo(TenantEnvironmentV2Entity):
 
     entityuri = "/slo"
     list_attr = "slo"
+    has_id = True
     uri = TenantEnvironmentV2Entity.uri + entityuri
 
     def __init__(self, **kwargs):
         TenantEnvironmentV2Entity.__init__(self, **kwargs)
         self.apipath = self.uri
+
+    def getHttpMethod(self):
+        return "PUT" if self.entityid != "" else "POST"
 
     def getAll(self, session):
         pass
