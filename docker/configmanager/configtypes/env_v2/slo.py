0ffdace0662d508c3802b508ef5cb5ac7e7a529d
Reinhard Weber <r.brandstaedter@gmail.com>
2023-05-02 12:33:11 +0200

    bug: slo definitions are still via api2 and not apiv2/settings

--- docker/configmanager/configtypes/env_v2/slo.py
+++ docker/configmanager/configtypes/env_v2/slo.py
@@ -1,16 +1,16 @@
 
 ''' Dynatrace Settings V2 API Support '''
-from ..ConfigTypes import TenantEnvironmentV2Setting
+from ..ConfigTypes import TenantEnvironmentV2Entity
 
 
-class slo(TenantEnvironmentV2Setting):
+class slo(TenantEnvironmentV2Entity):
     ''' Dynatrace Settings V2 API'''
 
     entityuri = "/slo"
-    uri = TenantEnvironmentV2Setting.uri + entityuri
+    uri = TenantEnvironmentV2Entity.uri + entityuri
 
     def __init__(self, **kwargs):
-        TenantEnvironmentV2Setting.__init__(self, **kwargs)
+        TenantEnvironmentV2Entity.__init__(self, **kwargs)
         self.apipath = self.uri
 
     def getAll(self, session):
