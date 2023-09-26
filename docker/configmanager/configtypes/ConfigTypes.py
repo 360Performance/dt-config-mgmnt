3ee6216d26ae8a4102914c698d060b62f3381ca9
Reinhard Weber <r.brandstaedter@gmail.com>
2023-09-26 12:23:44 +0200

    no import

--- docker/configmanager/configtypes/ConfigTypes.py
+++ docker/configmanager/configtypes/ConfigTypes.py
@@ -5,10 +5,10 @@ import json
 import hashlib
 import uuid
 import importlib
-import sys
-sys.path.append("..")
-from ..utils import deep_get
+from functools import reduce
 
+def deep_get(dictionary, keys, default=None):
+    return reduce(lambda d, key: d.get(key, default) if isinstance(d, dict) else default, keys.split("."), dictionary)
 
 
 # LOG CONFIGURATION
