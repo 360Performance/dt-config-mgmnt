3323c48166f165b416ccd3dba4022f38d5ebcae2
Reinhard Weber (Brandstaedter) <r.brandstaedter@gmail.com>
2023-09-26 11:35:20 +0200

    Fix dashbaords (#66)
    * use deep_get everywhere

--- docker/configmanager/configmanager.py
+++ docker/configmanager/configmanager.py
@@ -210,7 +210,7 @@ def getConfigSettings(entitytypes, entityconfig, parameters, dumpconfig):
                         logger.debug(json.dumps(entity, indent=2, separators=(",", ": ")))
                         logger.error(traceback.print_exc())
                     logger.debug(
-                        f'{eType.__name__} {entity[eType.id_attr]} ({entity[eType.name_attr]}) of {c_id}::{t_id}:\n{json.dumps(entity, indent=2, separators=(",", ": "))}')
+                        f'{eType.__name__} {deep_get(entity,eType.id_attr)} ({deep_get(entity,eType.name_attr)}) of {c_id}::{t_id}:\n{json.dumps(entity, indent=2, separators=(",", ": "))}')
                     if dumpconfig:
                         centity.dumpDTO(config_dump_dir)
                         entity_definition = centity.getConfigDefinition()
