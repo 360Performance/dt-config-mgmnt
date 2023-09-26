d0dfd48fe38cd654b48c9769629fa829a7d17d21
Reinhard Weber (Brandstaedter) <r.brandstaedter@gmail.com>
2023-09-26 10:38:14 +0200

    Azurecredentials (#61)
    * Dev (#54)
    
    * bug: avoid dumping of empty dtos
    
    * bug: slo definitions are still via api2 and not apiv2/settings
    
    * bug: add list attribute for slo type
    
    * cleanup DTO dump path
    
    * adding a default pageSize parameter for get API
    
    * adding default pagesize parameter to GET Apis
    
    * schema update ID for SLOS
    
    * update schema for SLO definition
    
    * spceial case SLOs have an ID but can't be put with a custom ID
    
    * selecting proper HTTP method for SLO
    
    * setting entityid for SLO Api
    
    * smart handling of SLO configs, using DT generated IDs (#47)
    
    * SLO Definition handling (#48) (#49)
    
    * bug: avoid dumping of empty dtos
    
    * bug: slo definitions are still via api2 and not apiv2/settings
    
    * bug: add list attribute for slo type
    
    * cleanup DTO dump path
    
    * adding a default pageSize parameter for get API
    
    * adding default pagesize parameter to GET Apis
    
    * schema update ID for SLOS
    
    * update schema for SLO definition
    
    * spceial case SLOs have an ID but can't be put with a custom ID
    
    * selecting proper HTTP method for SLO
    
    * setting entityid for SLO Api
    
    * smart handling of SLO configs, using DT generated IDs (#47)
    
    * Introducing Pre-Post Hooks (#50)
    
    * fixing for settings api v2 objects
    
    * first version of updateing an existing K8s config via prePOST hook
    
    * support for multiple prePOST hooks
    
    * disabling of old k8s cluster monitoring settings by name
    
    * Pre post hooks (#53)
    
    * fixing for settings api v2 objects
    
    * first version of updateing an existing K8s config via prePOST hook
    
    * support for multiple prePOST hooks
    
    * disabling of old k8s cluster monitoring settings by name
    
    * bug: hooks shoud be optional
    
    * bug: wrong classpath created for v1 config api objects (#56)
    
    * bug: classpath messed up when for 'classic' API endpoints (#55)
    
    * dev to master (#58)
    
    * key user actions require POST not PUT (#57)
    
    * Dev (#60)
    
    * mobile apps should be PUT nost POSTed (#59)
    
    * correct name attribute for azure credentials

--- docker/configmanager/configtypes/config_v1/azurecredentials.py
+++ docker/configmanager/configtypes/config_v1/azurecredentials.py
@@ -2,5 +2,6 @@ from ..ConfigTypes import TenantConfigV1Entity
 
 
 class azurecredentials(TenantConfigV1Entity):
+    name_attr = "label"
     entityuri = "/azure/credentials"
     uri = TenantConfigV1Entity.uri + entityuri
